302613a0a3cf3c968e09b4b2f810439fbef2c4ec
unknown <pramesh@generaltechnology.com.np>
2025-03-04 22:18:03 +0545

    EAZ-7: Implement Header, Footer And Base Architecture

---
+++ src/app/pages/home/page.jsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-4xl font-bold text-center">This is a home page</h1>
+    </div>
+  );
+}
---
+++ src/app/pages/profile/page.jsx
@@ -0,0 +1,7 @@
+export default function Profile() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-4xl font-bold text-center">This is a Profile Page</h1>
+    </div>
+  );
+}
